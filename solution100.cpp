9ff715c731d1b328e20fc1bdb93257d9838b77ee
TheMagicFlute <The_Magic_Flute@outlook.com>
2023-06-18 22:07:23 +0800

    solu100

---
+++ solution100.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    unsigned n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        unsigned x = i;
+        while (x)
+        {
+            if (x % 10 == 7 || i % 7 == 0)
+            {
+                cout << i << " ";
+                break;
+            }
+            x /= 10;
+        }
+    }
+    cout << endl;
+    return 0;
+}
