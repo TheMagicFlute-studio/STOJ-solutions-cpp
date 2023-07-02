66f62d8f174ca13277a69ed9ab25cda8532f8e31
TheMagicFlute <The_Magic_Flute@outlook.com>
2023-07-02 17:59:40 +0800

    update solu118

--- solution118.cpp
+++ solution118.cpp
@@ -1,26 +1,37 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main() 
+
+int main(int argc, char *argv[])
 {
 	int n, first, second;
+
 	cin >> n;
+
 	int a[10000];
-	for (int i = 0; i < n; i ++)
+
+	for (int i = 0; i < n; i++)
 	{
 		cin >> a[i];
 	}
+
 	first = a[n - 1];
+
 	second = a[0];
-	for (int i = n - 1; i > 1; i --)
+
+	for (int i = n - 1; i > 1; i--)
 	{
 		a[i] = a[i - 1];
 	}
+
 	a[0] = first;
+
 	a[1] = second;
-	for (int i = 0; i < n; i ++)
+
+	for (int i = 0; i < n; i++)
 	{
 		cout << a[i] << " ";
 	}
+
 	return 0;
 }
-
