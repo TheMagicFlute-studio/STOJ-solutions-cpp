10763261fb4bab633e9441e1ad8d9227f7862ff8
TheMagicFlute <The_Magic_Flute@outlook.com>
2023-07-16 19:28:00 +0800

    update solu134

--- solution134.cpp
+++ solution134.cpp
@@ -27,3 +27,38 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+/* solution 2
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int n, i, j, a[100] = {0};
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int max = 0, max_p = 0;
+    for (i = 0; i < n - 1; i++)
+    {
+        max = a[i], max_p = i;
+        for (j = i; j < n - 1; j++)
+        {
+            if (a[j + 1] > max)
+            {
+                max = a[j + 1];
+                max_p = j + 1;
+            }
+        }
+        swap(a[i], a[max_p]);
+    }
+    for (i = n; n >= 0; n--)
+    {
+        cout << a[n] << " ";
+    }
+}
+
+*/
